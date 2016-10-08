5dc72e21675fbd1144509de6660c59b8afc05553
soft-TOP <TorstenPohling@soft-TOP.de>
2016-10-08 15:19:53 +0200

    Beispiel
    zeigt die verwendung von Task

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskWaitAny
+{
+    class Program
+    {
+        static void Main()
+        {
+            Task<int>[] tasks = new Task<int>[3];
+            tasks[0] = Task.Run(() => { Thread.Sleep(2000); return 1; });
+            tasks[1] = Task.Run(() => { Thread.Sleep(1000); return 2; });
+            tasks[2] = Task.Run(() => { Thread.Sleep(3000); return 3; });
+
+            while (0 < tasks.Length)
+            {
+                Int32 i = Task.WaitAny(tasks);
+                Task<int> completedTask = tasks[i];
+                Console.WriteLine(completedTask.Result);
+
+                var temp = tasks.ToList();
+                temp.RemoveAt(i);
+                tasks = temp.ToArray();
+            }
+            Console.ReadLine();
+        }
+    }
+}
